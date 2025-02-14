85b18669180e3901e734b2346cd83a9160e78b94
notwld <mwfarrukh@gmail.com>
2025-02-14 17:00:16 +0500

    init

---
+++ cms/src/api/vegetable/controllers/vegetable.ts
@@ -0,0 +1,7 @@
+/**
+ * vegetable controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::vegetable.vegetable');
---
+++ cms/src/api/vegetable/services/vegetable.ts
@@ -0,0 +1,7 @@
+/**
+ * vegetable service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::vegetable.vegetable');
